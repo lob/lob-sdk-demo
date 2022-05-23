d3c694f6dd12115295647cf31fdff7811fd4afa1
Matthew Jackson <mwj8410@gmail.com>
2022-05-23 13:57:08 -0500

    chore(java) stub remaining resources

--- lob-java/src/main/java/com/example/RouteControllers.java
+++ lob-java/src/main/java/com/example/RouteControllers.java
@@ -4,8 +4,7 @@ import com.lob.api.ApiClient;
 import com.lob.api.ApiException;
 import com.lob.api.Configuration;
 import com.lob.api.auth.HttpBasicAuth;
-import com.lob.api.client.AddressesApi;
-import com.lob.api.client.BankAccountsApi;
+import com.lob.api.client.*;
 import org.json.JSONObject;
 import org.openapitools.client.model.*;
 import org.springframework.http.HttpStatus;
@@ -23,7 +22,6 @@ public class RouteControllers {
 
     RouteControllers() {
         ApiClient client = Configuration.getDefaultApiClient();
-        // defaultClient.setBasePath("https://api.lob.com/v1");
         HttpBasicAuth basicAuth = (HttpBasicAuth) client.getAuthentication("basicAuth");
         basicAuth.setUsername(System.getenv("LOB_API_TEST_KEY"));
 
@@ -57,7 +55,13 @@ public class RouteControllers {
         responseBody.setVersion("13.0.0");
         responseBody.setSupportedResources(new String[]{
                 "ADDRESSES",
-                "BANK_ACCOUNTS"
+                "BANK_ACCOUNTS",
+                "CHECKS",
+                "LETTERS",
+                "POSTCARDS",
+                "SELF_MAILERS",
+                "TEMPLATES",
+                "TEMPLATE_VERSIONS"
         });
 
         ObjectMapper objectMapper = new ObjectMapper();
@@ -76,7 +80,6 @@ public class RouteControllers {
     @RequestMapping(
             value = "/addresses",
             method = RequestMethod.POST,
-            produces = "application/json",
             consumes = "application/json"
     )
     @ResponseBody
@@ -166,6 +169,45 @@ public class RouteControllers {
 //        } catch (ApiException e) {
 //            e.printStackTrace();
 //        }
+
+        return new ResponseEntity<String>(jsonInString, status);
+    }
+
+    @RequestMapping(
+            value = "/address",
+            method = RequestMethod.POST,
+            consumes = "application/json"
+    )
+    @ResponseBody
+    public ResponseEntity<String> address(@RequestBody String body) {
+        AddressesApi apiInstance = new AddressesApi(this.lobClient);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonInString = "{}";
+        HttpStatus status = HttpStatus.OK;
+        try {
+            // Read input values
+            AddressEditable addressEditable = new AddressEditable();
+            JSONObject inputObj = new JSONObject(body);
+
+            addressEditable.setName(inputObj.getString("name"));
+            addressEditable.setAddressLine1(inputObj.getString("address_line1"));
+            if (inputObj.has("address_line2")) {
+                addressEditable.setAddressLine2(inputObj.getString("address_line2"));
+            }
+            addressEditable.setAddressCity(inputObj.getString("address_city"));
+            addressEditable.setAddressState(inputObj.getString("address_state"));
+            addressEditable.setAddressZip(inputObj.getString("address_zip"));
+
+            // Operations
+            Address address = apiInstance.create(addressEditable);
+
+            jsonInString = objectMapper.writeValueAsString(address);
+
+        } catch (IOException | ApiException e) {
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+            e.printStackTrace();
+        }
 
         return new ResponseEntity<String>(jsonInString, status);
     }
@@ -272,30 +314,13 @@ public class RouteControllers {
     }
 
     @RequestMapping(
-            value = "/checks",
+            value = "/check_bank_accounts",
             method = RequestMethod.POST,
             produces = "application/json",
             consumes = "application/json"
     )
     @ResponseBody
-    public ResponseEntity<String> checks(@RequestBody String body) {
-        class ChecksResponse {
-            private BankAccount createdBankAccount;
-            public BankAccount getCreatedBankAccount() { return this.createdBankAccount; }
-            public void setCreatedBankAccount(BankAccount address) { this.createdBankAccount = address; }
-
-            private BankAccount retrievedBankAccount;
-            BankAccount getRetrievedBankAccount() { return this.retrievedBankAccount; }
-            public void setRetrievedBankAccount(BankAccount account) { this.retrievedBankAccount = account; }
-
-            private BankAccountList listedBankAccounts;
-            public BankAccountList getListedAddresses() { return this.listedBankAccounts; }
-            public void setListedBankAccount(BankAccountList accounts) { this.listedBankAccounts = accounts; }
-
-            private BankAccountDeletion deletedBankAccount;
-            public BankAccountDeletion getDeletedBankAccount() { return this.deletedBankAccount; }
-            public void setDeletedBankAccount(BankAccountDeletion account) { this.deletedBankAccount = account; }
-        }
+    public ResponseEntity<String> bankAccount(@RequestBody String body) {
         BankAccountsApi apiInstance = new BankAccountsApi(this.lobClient);
 
         ObjectMapper objectMapper = new ObjectMapper();
@@ -316,13 +341,241 @@ public class RouteControllers {
             bankAccountWritable.setAccountType(bankAccountType);
 
             // Operations
+            BankAccount bankAccount = apiInstance.create(bankAccountWritable);
+
+            jsonInString = objectMapper.writeValueAsString(bankAccount);
+        } catch (IOException | ApiException e) {
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+            e.printStackTrace();
+        }
+
+        return new ResponseEntity<String>(jsonInString, status);
+    }
+
+    @RequestMapping(
+            value = "/checks",
+            method = RequestMethod.POST,
+            produces = "application/json",
+            consumes = "application/json"
+    )
+    @ResponseBody
+    public ResponseEntity<String> checks(@RequestBody String body) {
+        class ChecksResponse {
+            private Check createdCheck;
+            public Check getCreatedCheck() { return this.createdCheck; }
+            public void setCreatedCheck(Check check) { this.createdCheck = check; }
+
+            private Check retrievedCheck;
+            Check getRetrievedCheck() { return this.retrievedCheck; }
+            public void setRetrievedCheck(Check check) { this.retrievedCheck = check; }
+
+            private CheckList listedChecks;
+            public CheckList getListedChecks() { return this.listedChecks; }
+            public void setListedChecks(CheckList checks) { this.listedChecks = checks; }
+
+            private CheckDeletion deletedCheck;
+            public CheckDeletion getDeletedCheck() { return this.deletedCheck; }
+            public void setDeletedCheck(CheckDeletion check) { this.deletedCheck = check; }
+        }
+        ChecksApi apiInstance = new ChecksApi(this.lobClient);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonInString = "{}";
+        HttpStatus status = HttpStatus.OK;
+        try {
+            // Read input values
+            CheckEditable checkEditable = new CheckEditable();
+            JSONObject inputObj = new JSONObject(body);
+
+            checkEditable.setDescription(inputObj.getString("description"));
+
+            // Operations
             ChecksResponse responseBody = new ChecksResponse();
-            System.out.println(bankAccountWritable);
-            BankAccount ba = apiInstance.create(bankAccountWritable);
-            responseBody.setCreatedBankAccount(ba);
-            responseBody.setRetrievedBankAccount(apiInstance.get(responseBody.getCreatedBankAccount().getId()));
-            responseBody.setListedBankAccount(apiInstance.list(2, null, null, null, null, null));
-            responseBody.setDeletedBankAccount(apiInstance.delete(responseBody.getCreatedBankAccount().getId()));
+            Check check = apiInstance.create(checkEditable, null);
+            responseBody.setCreatedCheck(check);
+            responseBody.setRetrievedCheck(apiInstance.get(responseBody.getCreatedCheck().getId()));
+            responseBody.setListedChecks(apiInstance.list(2, null, null, null, null, null, null, null, null, null));
+            responseBody.setDeletedCheck(apiInstance.cancel(responseBody.getCreatedCheck().getId()));
+
+            jsonInString = objectMapper.writeValueAsString(responseBody);
+        } catch (IOException | ApiException e) {
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+            e.printStackTrace();
+        }
+
+//        try {
+//            AddressEditable addressRaw = new AddressEditable();
+//            addressRaw.setName("Harry Zhang");
+//            addressRaw.setCompany("Lob");
+//            addressRaw.setEmail("harry@lob.com");
+//            addressRaw.setPhone("5555555555");
+//            addressRaw.setAddressLine1("210 King St");
+//            addressRaw.setAddressLine2("# 6100");
+//            addressRaw.setAddressCity("San Francisco");
+//            addressRaw.setAddressState("CA");
+//            addressRaw.setAddressZip("94107");
+//            addressRaw.setAddressCountry(CountryExtended.US);
+//
+//            Address address = apiInstance.create(addressRaw);
+//        } catch (ApiException e) {
+//            e.printStackTrace();
+//        }
+
+//        try {
+//            Address address = apiInstance.get("adr_fa85158b26c3eb7c");
+//        } catch (ApiException e) {
+//            e.printStackTrace();
+//        }
+
+//        try {
+//            AddressList addresses = apiInstance.list(2, null, null, null, null, null);
+//        } catch (ApiException e) {
+//            e.printStackTrace();
+//        }
+
+//        try {
+//            AddressDeletion address = apiInstance.delete("adr_fa85158b26c3eb7c");
+//        } catch (ApiException e) {
+//            e.printStackTrace();
+//        }
+
+        return new ResponseEntity<String>(jsonInString, status);
+    }
+
+    @RequestMapping(
+            value = "/letters",
+            method = RequestMethod.POST,
+            produces = "application/json",
+            consumes = "application/json"
+    )
+    @ResponseBody
+    public ResponseEntity<String> letters(@RequestBody String body) {
+        class LettersResponse {
+            private Letter createdLetter;
+            public Letter getCreatedLetter() { return this.createdLetter; }
+            public void setCreatedLetter(Letter letter) { this.createdLetter = letter; }
+
+            private Letter retrievedLetter;
+            Letter getRetrievedLetter() { return this.retrievedLetter; }
+            public void setRetrievedLetter(Letter letter) { this.retrievedLetter = letter; }
+
+            private LetterList listedLetters;
+            public LetterList getListedLetters() { return this.listedLetters; }
+            public void setListedLetters(LetterList letters) { this.listedLetters = letters; }
+
+            private LetterDeletion deletedLetter;
+            public LetterDeletion getDeletedLetter() { return this.deletedLetter; }
+            public void setDeletedLetter(LetterDeletion letter) { this.deletedLetter = letter; }
+        }
+        LettersApi apiInstance = new LettersApi(this.lobClient);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonInString = "{}";
+        HttpStatus status = HttpStatus.OK;
+        try {
+            // Read input values
+            LetterEditable letterEditable = new LetterEditable();
+            JSONObject inputObj = new JSONObject(body);
+
+            letterEditable.setDescription(inputObj.getString("description"));
+
+            // Operations
+            LettersResponse responseBody = new LettersResponse();
+            Letter letter = apiInstance.create(letterEditable, null);
+            responseBody.setCreatedLetter(letter);
+            responseBody.setRetrievedLetter(apiInstance.get(responseBody.getCreatedLetter().getId()));
+            responseBody.setListedLetters(apiInstance.list(2, null, null, null, null, null, null, null, null, null, null));
+            responseBody.setDeletedLetter(apiInstance.cancel(responseBody.getCreatedLetter().getId()));
+
+            jsonInString = objectMapper.writeValueAsString(responseBody);
+        } catch (IOException | ApiException e) {
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+            e.printStackTrace();
+        }
+
+//        try {
+//            AddressEditable addressRaw = new AddressEditable();
+//            addressRaw.setName("Harry Zhang");
+//            addressRaw.setCompany("Lob");
+//            addressRaw.setEmail("harry@lob.com");
+//            addressRaw.setPhone("5555555555");
+//            addressRaw.setAddressLine1("210 King St");
+//            addressRaw.setAddressLine2("# 6100");
+//            addressRaw.setAddressCity("San Francisco");
+//            addressRaw.setAddressState("CA");
+//            addressRaw.setAddressZip("94107");
+//            addressRaw.setAddressCountry(CountryExtended.US);
+//
+//            Address address = apiInstance.create(addressRaw);
+//        } catch (ApiException e) {
+//            e.printStackTrace();
+//        }
+
+//        try {
+//            Address address = apiInstance.get("adr_fa85158b26c3eb7c");
+//        } catch (ApiException e) {
+//            e.printStackTrace();
+//        }
+
+//        try {
+//            AddressList addresses = apiInstance.list(2, null, null, null, null, null);
+//        } catch (ApiException e) {
+//            e.printStackTrace();
+//        }
+
+//        try {
+//            AddressDeletion address = apiInstance.delete("adr_fa85158b26c3eb7c");
+//        } catch (ApiException e) {
+//            e.printStackTrace();
+//        }
+
+        return new ResponseEntity<String>(jsonInString, status);
+    }
+
+    @RequestMapping(
+            value = "/self_mailers",
+            method = RequestMethod.POST,
+            produces = "application/json",
+            consumes = "application/json"
+    )
+    @ResponseBody
+    public ResponseEntity<String> selfMailers(@RequestBody String body) {
+        class SelfMailersResponse {
+            private SelfMailer createdSelfMailer;
+            public SelfMailer getCreatedSelfMailer() { return this.createdSelfMailer; }
+            public void setCreatedSelfMailer(SelfMailer mailer) { this.createdSelfMailer = mailer; }
+
+            private SelfMailer retrievedSelfMailer;
+            SelfMailer getRetrievedSelfMailer() { return this.retrievedSelfMailer; }
+            public void setRetrievedSelfMailer(SelfMailer mailer) { this.retrievedSelfMailer = mailer; }
+
+            private SelfMailerList listedSelfMailers;
+            public SelfMailerList getListedSelfMailers() { return this.listedSelfMailers; }
+            public void setListedSelfMailers(SelfMailerList letters) { this.listedSelfMailers = letters; }
+
+            private SelfMailerDeletion deletedSelfMailer;
+            public SelfMailerDeletion getDeletedSelfMailer() { return this.deletedSelfMailer; }
+            public void setDeletedSelfMailer(SelfMailerDeletion letter) { this.deletedSelfMailer = letter; }
+        }
+        SelfMailersApi apiInstance = new SelfMailersApi(this.lobClient);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonInString = "{}";
+        HttpStatus status = HttpStatus.OK;
+        try {
+            // Read input values
+            SelfMailerEditable letterEditable = new SelfMailerEditable();
+            JSONObject inputObj = new JSONObject(body);
+
+            letterEditable.setDescription(inputObj.getString("description"));
+
+            // Operations
+            SelfMailersResponse responseBody = new SelfMailersResponse();
+            SelfMailer mailer = apiInstance.create(letterEditable, null);
+            responseBody.setCreatedSelfMailer(mailer);
+            responseBody.setRetrievedSelfMailer(apiInstance.get(responseBody.getCreatedSelfMailer().getId()));
+            responseBody.setListedSelfMailers(apiInstance.list(2, null, null, null, null, null, null, null, null, null, null));
+            responseBody.setDeletedSelfMailer(apiInstance.delete(responseBody.getCreatedSelfMailer().getId()));
 
             jsonInString = objectMapper.writeValueAsString(responseBody);
         } catch (IOException | ApiException e) {
