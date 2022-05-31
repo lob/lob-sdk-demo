6267bd873549d0ebbf2506056431f4352a405484
Matthew Jackson <mwj8410@gmail.com>
2022-05-31 15:03:42 -0500

    chore(java) remove stale commented code

--- lob-java/src/main/java/com/example/LobJavaDemo.java
+++ lob-java/src/main/java/com/example/LobJavaDemo.java
@@ -19,35 +19,6 @@ public class LobJavaDemo {
 
         demoApp.setDefaultProperties(Collections.singletonMap("server.port", port));
         demoApp.run(args);
-
-        /*
-            ApiClient defaultClient = Configuration.getConfigForIntegration();
-            defaultClient.setBasePath("https://api.lob.com/v1");
-            HttpBasicAuth basicAuth = (HttpBasicAuth) defaultClient.getAuthentication("basicAuth");
-
-            // If using a env file
-            // Dotenv dotenv = Dotenv.load();
-            // String apiKey = dotenv.get("LOB_API_TEST_KEY");
-
-            // basicAuth.setUsername(apiKey);
-
-            // if not using a env file
-            basicAuth.setUsername("YOUR-API-KEY");
-
-            AddressesApi apiInstance = new AddressesApi(defaultClient);
-            AddressEditable addressEditable = new AddressEditable(); // AddressEditable
-            try {
-                Address result = apiInstance.addressCreate(addressEditable);
-                System.out.println(result);
-            } catch (ApiException e) {
-                System.err.println("Exception when calling AddressesApi#addressCreate");
-                System.err.println("Status code: " + e.getCode());
-                System.err.println("Reason: " + e.getResponseBody());
-                System.err.println("Response headers: " + e.getResponseHeaders());
-                e.printStackTrace();
-            }
-
-         */
     }
 
     @Bean
